f757ba19c1d0a6ea8343dbd96581d77a6302cd53
Doug Waldron <douglas.waldron@dnr.ga.gov>
2022-03-14 16:53:32 -0400

    Change TFM to net6.0, remove unused files, and fix errors in DB Helper

--- DBHelper/My Project/AssemblyInfo.vb
+++ DBHelper/My Project/AssemblyInfo.vb
@@ -4,14 +4,13 @@ Imports System.Runtime.InteropServices
 
 <Assembly: AssemblyTitle("Simple DB Helper")>
 <Assembly: AssemblyDescription("A library to simplify database interactions")>
-<Assembly: AssemblyCompany("State of Georgia, EPD-IT, Doug Waldron")>
+<Assembly: AssemblyCompany("State of Georgia, EPD-IT")>
 <Assembly: AssemblyProduct("Simple DB Helper")>
-<Assembly: AssemblyCopyright("Copyright © 2019")> 
-<Assembly: AssemblyTrademark("")> 
+<Assembly: AssemblyCopyright("Copyright © 2022")>
 
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
 <Assembly: Guid("f5f75e37-861e-4a77-b6fc-32b5ea2ab3b6")>
 
-<Assembly: AssemblyVersion("3.0.2.0")>
+<Assembly: AssemblyVersion("4.0.0.0")>
